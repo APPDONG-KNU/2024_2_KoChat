f2ad0d4e7686d61978bc9f07f58ed7dea712b037
sunflow-er <antjd03170@gmail.com>
2024-10-02 14:39:14 +0900

    User 데이터 클래스 항목 추가

--- app/src/main/java/org/javaapp/chatting/userlist/User.kt
+++ app/src/main/java/org/javaapp/chatting/userlist/User.kt
@@ -1,6 +1,7 @@
 package org.javaapp.chatting.userlist
 
 data class User(
-    val  username : String,
-    val  statusMessage : String,
+    val  id : String, // 사용자 식별 ID
+    val  name : String, // 이름
+    val  statusMessage : String, // 상태메시지
 )
