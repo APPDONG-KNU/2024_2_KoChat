7f54ee9d83c3453b97a3ae2fb02402b6cd4d6226
sunflow-er <antjd03170@gmail.com>
2024-10-02 13:45:44 +0900

    3. UserListFragment 생성, User 데이터 클래스 생성

---
+++ app/src/main/java/org/javaapp/chatting/userlist/User.kt
@@ -0,0 +1,3 @@
+package org.javaapp.chatting.userlist
+
+data class User()
---
+++ app/src/main/java/org/javaapp/chatting/userlist/UserListFragment.kt
@@ -0,0 +1,8 @@
+package org.javaapp.chatting.userlist
+
+import androidx.fragment.app.Fragment
+
+class UserListFragment : Fragment() {
+
+
+}
