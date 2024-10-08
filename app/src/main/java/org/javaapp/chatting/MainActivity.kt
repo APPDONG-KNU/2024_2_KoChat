3da89a3fde7e776d2dc25da87d068ad82ebdb67a
sunflow-er <antjd03170@gmail.com>
2024-10-08 11:12:02 +0900

    (ChatRoomFragment 리사이클러뷰 구성 클래스 이름 수정)

--- app/src/main/java/org/javaapp/chatting/MainActivity.kt
+++ app/src/main/java/org/javaapp/chatting/MainActivity.kt
@@ -2,6 +2,7 @@ package org.javaapp.chatting
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
@@ -72,6 +73,8 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.fragment_container, fragment)
             commit()
         }
+
+
     }
 
 }
